63360a9656d5a5c7cbb77beea17e7b38e51eb139
Kard-py <caiodet654@gmail.com>
2022-11-25 18:10:06 -0400

    Up

--- pages/api/removePedido/[uid].js
+++ pages/api/removePedido/[uid].js
@@ -1,11 +1,11 @@
-import { uri, connectToDataBase } from "../../utils/mongodb"
+import { uri, connectToDataBase } from "../../../utils/mongodb"
 export default async function handler(req, res) {
   if(req.method != "DELETE"){
     res.status(405).json({msg: "Method not allowed"})
     return
   }
   
-  if(req.body == '' || req.body.remove_id == undefined || req.body.remove_id == null){
+  if(req.query == '' || req.query.uid == undefined || req.query.uid == null){
     res.status(406).json({msg: "Not Acceptable"})
     return
   }
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   const db = await connectToDataBase(uri)
   const collection = await db.collection("Pedidos")
 
-  const item = {uid: req.body.remove_id};
+  const item = {uid: req.query.uid};
 
   const r = await collection.deleteOne(item)
 
