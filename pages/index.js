e472b3bc2090964db428725887faeb2ec8548ac9
Kard-py <caiodet654@gmail.com>
2022-11-28 10:54:29 -0400

    Up

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home(){
         <Header/>
         <main className="flex flex-1 h-full w-full p-5 justify-center items-center ">
             <div className="flex flex-1 h-[550px] justify-center items-center">
-                <Link href="/menu-pedidos" className="select-none w-full m-1 bg-black h-full text-2xl font-bold text-white hover:bg-green-600 flex flex-1 justify-center items-center text-center duration-300 shadow-zinc-800 shadow-md">Cadastro<br/>De<br/>Pedidos</Link>
-                <Link href="/gerenciamento" className="select-none w-full m-1 bg-black h-full text-2xl font-bold text-white hover:bg-green-600 flex flex-1 justify-center items-center text-center duration-300 shadow-zinc-800 shadow-md">Gerenciamento<br/>Da<br/>Loja</Link>
+                <Link href="/menu-pedidos" className="select-none w-full m-1 bg-black h-full text-xl lg:text-2xl font-bold text-white hover:bg-green-600 flex flex-1 justify-center items-center text-center duration-300 shadow-zinc-800 shadow-md">Cadastro<br/>De<br/>Pedidos</Link>
+                <Link href="/gerenciamento" className="select-none w-full m-1 bg-black h-full text-xl lg:text-2xl font-bold text-white hover:bg-green-600 flex flex-1 justify-center items-center text-center duration-300 shadow-zinc-800 shadow-md">Gerenciamento<br/>Da<br/>Loja</Link>
             </div>
         </main>
         <Footer/>
