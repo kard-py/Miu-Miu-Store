492c0bc99df42e7027759ba90fbeaec861cdd54b
kard-py <caiodetz654@gmail.com>
2022-12-02 00:29:14 -0400

    up

--- pages/menu-pedidos/index.js
+++ pages/menu-pedidos/index.js
@@ -8,8 +8,18 @@ export default function Menu() {
       <main className="flex flex-1 h-full my-5 mx-5 justify-center items-center">
         <div className="flex flex-1 max-w-xl items-center justify-center">
           <div className="w-full h-full grid grid-rows-2 grid-cols-3 grid-flow-row gap-8 items-center justify-center">
-            <Link href="pulseiras" className="w-full bg-black h-24 text-sm text-white flex flex-1 justify-center items-center text-center duration-300 hover:bg-green-600 shadow-zinc-800 shadow-md">Pulseiras</Link>
-            {/* <Link href="tornozeleiras" className="w-full bg-black h-24 text-sm text-white flex flex-1 justify-center items-center text-center duration-300 hover:bg-green-600 shadow-zinc-800 shadow-md">Tornozeleiras</Link> */}
+            <Link
+              href="pulseiras"
+              className="w-full bg-black h-24 text-sm text-white flex flex-1 justify-center items-center text-center duration-300 hover:bg-green-600 shadow-zinc-800 shadow-md"
+            >
+              Pulseiras
+            </Link>
+            <Link
+              href="tornozeleiras"
+              className="w-full bg-black h-24 text-sm text-white flex flex-1 justify-center items-center text-center duration-300 hover:bg-green-600 shadow-zinc-800 shadow-md"
+            >
+              Tornozeleiras
+            </Link>
             {/* <Link href="chaveiros" className="w-full bg-black h-24 text-sm text-white flex flex-1 justify-center items-center text-center duration-300 hover:bg-green-600 shadow-zinc-800 shadow-md">Chaveiros</Link> */}
             {/* <Link href="coleiras" className="w-full bg-black h-24 text-sm text-white flex flex-1 justify-center items-center text-center duration-300 hover:bg-green-600 shadow-zinc-800 shadow-md">Coleiras</Link> */}
           </div>
@@ -17,5 +27,5 @@ export default function Menu() {
       </main>
       <Footer />
     </>
-  )
+  );
 }
